4c06e43b0cc6c7825c59114e28263ab646a94633
ciel-20 <najlalqina@gmail.com>
2025-04-17 21:40:30 +0700

    Fixed Database path to each sensor structure

--- Connection.py
+++ Connection.py
@@ -5,39 +5,60 @@ from datetime import datetime
 # MQTT settings
 broker = "broker.emqx.io"
 port = 1883
-topic = "TestforIoTBox"  # <-- match this to your ESP32 topic
+fire = "fire"  # Topic for fire events
+water = "water"  # Topic for water events
+
 
 # MySQL settings
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="",  # replace if you set a password in XAMPP
-    database="iotbox"
+    password="",  # Replace if you set a password in XAMPP
+    database="iotboxx"
 )
 cursor = db.cursor()
 
-# Called when connected to broker
+# Called when connected to the broker
 def on_connect(client, userdata, flags, rc):
     print("Connected to MQTT Broker!")
-    client.subscribe(topic)
+    client.subscribe([(fire, 0), (water, 0),])
 
 # Called when a message is received
 def on_message(client, userdata, msg):
     message = msg.payload.decode()
     print(f"Received: {message} on topic {msg.topic}")
 
-    # Insert into database
-    sql = "INSERT INTO fire_events (status, topic) VALUES (%s, %s)"
-    val = (message, msg.topic)
-    cursor.execute(sql, val)
-    db.commit()
+    if msg.topic == fire:
+        # Insert into fire_events table
+        sql = "INSERT INTO fire_events (status, topic) VALUES (%s, %s)"
+        val = (message, msg.topic)
+        cursor.execute(sql, val)
+        db.commit()
+        print("Saved to fire_events ✅")
 
-    print("Saved to database ✅")
+    elif msg.topic == water:
+        # Insert into water_events table
+        sql = "INSERT INTO water_events (status, topic) VALUES (%s, %s)"
+        val = (message, msg.topic)
+        cursor.execute(sql, val)
+        db.commit()
+        print("Saved to water_events ✅")
+
 
 # MQTT setup
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect(broker, port, 60)
-client.loop_forever()
+try:
+    client.connect(broker, port, 60)
+    client.loop_forever()
+
+except KeyboardInterrupt:
+    print("Disconnected manually.")
+
+finally:
+    if db.is_connected():
+        cursor.close()
+        db.close()
+        print("Database connection closed.")
